6fe11ed9c21a7db5a5e4ef6d55e6e3ef39bab7e8
Remix Workspace <willhanchen@gmail.com>
2022-10-07 18:59:11 +0800

    [feat] add Ep4 basic

---
+++ Ep4/example.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Example {
+    function TimeUnitExample() public pure {
+        // 1秒
+        assert(1 seconds == 1);
+
+        // 1分鐘 = 60秒
+        assert(1 minutes == 60 seconds);
+
+        // 1小時 = 60分鐘
+        assert(1 hours == 60 minutes);
+
+        // 1天=24小時
+        assert(1 days == 24 hours);
+
+        // 1周 = 7天
+        assert(1 weeks == 7 days);
+    }
+}
