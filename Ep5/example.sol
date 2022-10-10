938d808eec4077d7c3a157ee9e6c52bc2dbea364
Remix Workspace <willhanchen@gmail.com>
2022-10-11 06:30:35 +0800

    [feat] add Ep5 category

--- Ep5/example.sol
+++ Ep5/example.sol
@@ -9,9 +9,9 @@ contract Example {
 
     function withdraw(uint amount) public pure {
         uint total = 100;
-        /// @dev 數量必須要是正數
+        /// @dev 提款的數量必須低於總額度
         /// @notice 使用require()， 當條件不滿足時將退回剩下的瓦斯費，所以通常會放在前面
-        require(amount >= 0, "invalid uint");
+        require(amount <= total, "amount must less then total");
 
         /// @dev 提款的金額不能大於存款
         /// @notice 若提出的金額超過存款總額，則退回撤銷並退回狀態。
@@ -19,6 +19,7 @@ contract Example {
             revert NotEnoughFunds(amount, total);
         }
 
-        assert(amount <= total);
+        /// @dev 提款的額度必須是正整數
+        assert(amount > 0);
     }
 }
