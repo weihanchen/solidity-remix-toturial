3c83a762417ff4655e5e70d2b280a40c19c7eda9
Remix Workspace <willhanchen@gmail.com>
2022-10-18 21:19:42 +0800

    [feat] current interface usage

--- Ep10/example.sol
+++ Ep10/example.sol
@@ -2,14 +2,27 @@
 pragma solidity ^0.8.7.0;
 
 // 汽車的介面
-interface Car {
-    // 汽車的可以進行驅動
-    // !!! 這邊必須以external進行可視範圍的標示，因為對於介面來說就是外部可視
-    function drive() public pure;
+interface ICar {
+    /// @notice 汽車的可以進行驅動
+    /// @dev !!! 這邊必須以external進行可視範圍的標示，因為對於介面來說就是外部可視
+    function drive() external returns (string memory);
 }
 
-contract Truck is Car {
-    function drive() public pure {
-
+contract Truck is ICar {
+    function drive() public pure override returns (string memory){
+        return "Truck";
     }
+}
+
+
+interface ParentA {
+    function weight() external returns (uint256);
+}
+
+interface ParentB {
+    function weight() external returns (uint256);
+}
+
+interface Child is ParentA, ParentB {
+    function weight() external override(ParentA, ParentB) returns (uint256);
 }
